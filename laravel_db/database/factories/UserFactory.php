100dd26b1f0e1a95a8981c3b7381c594798cb078
salpurn <salsabilaprnm@gmail.com>
2025-10-21 19:31:58 +0700

    update

--- laravel_db/database/factories/UserFactory.php
+++ laravel_db/database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Database\Eloquent\Factories\WithFaker;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
@@ -17,8 +16,6 @@ class UserFactory extends Factory
      */
     protected static ?string $password;
 
-    use WithFaker;
-    
     /**
      * Define the model's default state.
      *
@@ -27,9 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name' => $this->faker->firstName(),
-            'last_name'  => $this->faker->lastName(),
-            'email'      => $this->faker->unique()->safeEmail(),
+            'first_name' => fake()->firstName(),
+            'last_name'  => fake()->lastName(),
+            'email'      => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password'   => bcrypt('password'), // password default
             'remember_token' => Str::random(10),
