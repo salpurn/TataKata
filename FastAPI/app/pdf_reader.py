0f971f4e64ed14411d0cb0178071a7452f0756cb
5025231146_Salsabila Purnama <162265407+salpurn@users.noreply.github.com>
2025-10-21 16:50:22 +0700

    Delete FastAPI directory

--- FastAPI/app/pdf_reader.py
+++
@@ -1,9 +0,0 @@
-import fitz  # PyMuPDF
-
-def extract_text_from_pdf(content: bytes) -> str:
-    try:
-        doc = fitz.open(stream=content, filetype="pdf")
-        full_text = "".join([page.get_text() for page in doc])
-        return full_text
-    except Exception as e:
-        return ""
